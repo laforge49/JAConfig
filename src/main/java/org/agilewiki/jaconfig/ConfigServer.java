5c8570f50c43919867150173b669afd7deee9ca5
Bill La Forge <laforge49@gmail.com>
2012-12-25 12:34:56 +0530

    reading/creating root jid

--- src/main/java/org/agilewiki/jaconfig/ConfigServer.java
+++ src/main/java/org/agilewiki/jaconfig/ConfigServer.java
@@ -24,41 +24,66 @@
 package org.agilewiki.jaconfig;
 
 import org.agilewiki.jactor.RP;
+import org.agilewiki.jactor.factory.JAFactory;
 import org.agilewiki.jasocket.jid.PrintJid;
-import org.agilewiki.jasocket.server.Server;
 import org.agilewiki.jasocket.node.Node;
+import org.agilewiki.jasocket.server.Server;
 import org.agilewiki.jfile.JFile;
 import org.agilewiki.jfile.JFileFactories;
+import org.agilewiki.jfile.block.Block;
+import org.agilewiki.jfile.block.LA32Block;
+import org.agilewiki.jid.JidFactories;
+import org.agilewiki.jid.scalar.vlens.actor.RootJid;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
 public class ConfigServer extends Server {
-    private JFile configDB;
+    private JFile dbFile;
+    private RootJid rootJid;
 
     @Override
     protected String serverName() {
         return "configServer";
     }
 
+    protected int maxSize() {
+        return 1000000000;
+    }
+
     @Override
     protected void startServer(final PrintJid out, final RP rp) throws Exception {
-        (new JFileFactories()).initialize(node().factory());
+        (new JFileFactories()).initialize(getParent());
         Path dbPath = new File(node().nodeDirectory(), "config.db").toPath();
-        configDB = new JFile();
-        configDB.initialize(getMailboxFactory().createAsyncMailbox());
-        configDB.open(
+        dbFile = new JFile();
+        dbFile.initialize(getMailboxFactory().createAsyncMailbox());
+        dbFile.open(
                 dbPath,
                 StandardOpenOption.READ,
                 StandardOpenOption.WRITE,
                 StandardOpenOption.CREATE);
+        Block block0 = newDbBlock();
+        block0.setCurrentPosition(0L);
+        block0.setFileName(dbPath.toString());
+        dbFile.readRootJid(block0, maxSize());
+        if (block0.isEmpty()) {
+            JAFactory factory = (JAFactory) getAncestor(JAFactory.class);
+            block0.setRootJid(
+                    (RootJid) factory.newActor(
+                            JidFactories.ROOT_JID_TYPE,
+                            getMailboxFactory().createMailbox(),
+                            dbFile));
+        }
+        rootJid = block0.getRootJid(getMailboxFactory().createMailbox(), getParent());
     }
 
     @Override
     public void close() {
-        if (configDB != null)
-            configDB.close();
+        if (dbFile != null) {
+            dbFile.close();
+            dbFile = null;
+        }
         super.close();
     }
 
@@ -71,5 +96,9 @@ public class ConfigServer extends Server {
             node.mailboxFactory().close();
             throw ex;
         }
+    }
+
+    protected Block newDbBlock() {
+        return new LA32Block();
     }
 }
